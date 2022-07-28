53d1a1a52db82e04315bbd8ccc30efc19aa2cbd4
Panagiotis Makris <panagiotis.makris@atcom.gr>
2022-07-28 23:53:45 +0300

    change server details

--- src/main/kotlin/gr/makris/smartConnect/data/server/ServerCheck.kt
+++ src/main/kotlin/gr/makris/smartConnect/data/server/ServerCheck.kt
@@ -3,5 +3,5 @@ package gr.makris.smartConnect.data.server
 data class ServerCheck(
     var serverName: String = "SmartConnectServer",
     var serverVersion: String = "1.0.0",
-    var serverStatus: String = "OK"
+    var serverStatus: String = "UP_RUNNING"
 )
